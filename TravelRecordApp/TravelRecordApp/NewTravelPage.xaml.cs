284fd12bf00e0dd29ca3e1314f8fafc87df423fc
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2021-12-31 22:07:52 +0100

    Add functionality to save experience

--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,28 @@ namespace TravelRecordApp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void tiSave_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+            };
+
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+            connection.CreateTable<Post>();
+            int rows = connection.Insert(post);
+            connection.Close();
+
+            if (rows > 0)
+            {
+                DisplayAlert("Success", "Experience successfully inserted", "OK");
+            }
+            else
+            {
+                DisplayAlert("Failure", "Experience failed to be insert", "OK");
+            }
         }
     }
 }
